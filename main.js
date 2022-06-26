d34faf8c6e7a494d2710354a86594c597193a104
Enderman-Somnath <91421328+Enderman-Somnath@users.noreply.github.com>
2022-06-26 19:39:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("Console is for developers");
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(400, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 300);
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet Has Initialized");
+}
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+        console.log("noseX=" + results[0].pose.nose.x);
+        console.log("noseY=" + results[0].pose.nose.y);
+    }
+}
+function draw(){
+    image(video, 0, 0, 400, 300);
+}
+function take_snapshot(){
+    save('Picture.png');
+}
